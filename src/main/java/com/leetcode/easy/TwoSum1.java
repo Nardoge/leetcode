549fe95cdcf10c9955e143fb97bd587d1fec7b51
nardoge <nardoge@github.com>
2020-04-19 11:18:31 +0800

    feat():twoSum 第一次解题，未优化

--- src/main/java/com/leetcode/easy/TwoSum1.java
+++ src/main/java/com/leetcode/easy/TwoSum1.java
@@ -9,7 +9,7 @@ package com.leetcode.easy;
 /**
  *  给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
  *
- * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+ *  你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
  *
  *  
  *
@@ -25,9 +25,40 @@ package com.leetcode.easy;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class TwoSum1 {
-    public int[] towSum(int[] nums,int target){
-        int[] subscript = new int[2];
 
-        return subscript;
+    public int[] towSum(int[] nums,int target){
+        int[] subscriptArr = new int[2];
+        if(nums.length < 2){
+            return subscriptArr;
+        } else {
+
+            for(int i = 0; i < nums.length; i++) {
+              int j = 1;
+                for(; j < nums.length; j++) {
+                    if (nums[i] + nums[j] == target && i != j){
+                        subscriptArr[0] = i;
+                        subscriptArr[1] = j;
+                        return subscriptArr;
+                    }
+                }
+            }
+        }
+
+        return subscriptArr;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,5,5,11};
+        TwoSum1 twoSum1 = new TwoSum1();
+        int[] subscripArr = twoSum1.towSum(nums,10);
+        System.out.println(subscripArr[0]);
+        System.out.println(subscripArr[1]);
+
+        int[] nums2 = {2,7,11,15};
+        TwoSum1 twoSum3 = new TwoSum1();
+        int[] subscripArr2 = twoSum3.towSum(nums2,9);
+        System.out.println(subscripArr2[0]);
+        System.out.println(subscripArr2[1]);
+
     }
 }
